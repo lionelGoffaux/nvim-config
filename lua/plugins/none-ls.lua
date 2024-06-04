00b8509ec73a7d85c1a6f25bb62dd3a7f2e37bd6
lgoffaux <contact@lionel-goffaux.be>
2024-06-04 14:28:44 +0200

    fix: switch to none-ls-extra

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,16 @@
 return {
 	"nvimtools/none-ls.nvim",
+  dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+  },
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
-        null_ls.builtins.diagnostics.ruff,
-        null_ls.builtins.diagnostics.flake8,
+        require("none-ls.diagnostics.ruff"),
+        require("none-ls.diagnostics.flake8"),
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.clang_format,
 			},
