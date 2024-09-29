ba9e47a13291b97735b38f01b10c9f1d14bf0594
Lionel Goffaux <contact@lionel-goffaux.be>
2024-09-29 11:05:39 +0200

    feat: add system clipboard shortcuts

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,6 +10,18 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>Y', '"+yg_', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>yy', '"+yy', { noremap = true, silent = true })
+
+-- Paste from clipboard
+vim.keymap.set('n', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>P', '"+P', { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>p', '"+p', { noremap = true, silent = true })
+vim.keymap.set('v', '<leader>P', '"+P', { noremap = true, silent = true })
+
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
