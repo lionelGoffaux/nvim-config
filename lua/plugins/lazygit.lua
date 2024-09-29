634021247521676d37d0def43c9ac93a8c8cfe2e
Lionel Goffaux <contact@lionel-goffaux.be>
2024-09-29 13:12:26 +0200

    feat: add lazygit shotcut

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,4 +1,7 @@
-  return {
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-  }
+return {
+  "kdheepak/lazygit.nvim",
+  lazy = false,
+  config = function()
+    vim.keymap.set("n", "<leader>lg", ":LazyGit<CR>", { silent = true })
+  end,
+}
