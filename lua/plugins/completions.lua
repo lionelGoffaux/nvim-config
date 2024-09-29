86208b7b34b845c2405539a5ca6c3327bd6e0897
Lionel Goffaux <contact@lionel-goffaux.be>
2024-09-29 10:54:16 +0200

    chore: remove unused code

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -12,14 +12,7 @@ return {
 	{
 		"hrsh7th/nvim-cmp",
 		config = function()
-			local has_words_before = function()
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
-
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
