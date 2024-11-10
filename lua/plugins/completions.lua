eb7e1267e83ddc99e26874450d222ce882916c49
Lionel Goffaux <contact@lionel-goffaux.be>
2024-11-10 21:41:12 +0100

    feat: add snip navigation

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -8,6 +8,25 @@ return {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
+		config = function()
+			local ls = require("luasnip")
+
+			vim.keymap.set({ "i" }, "<C-K>", function()
+				ls.expand()
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-L>", function()
+				ls.jump(1)
+			end, { silent = true })
+			vim.keymap.set({ "i", "s" }, "<C-J>", function()
+				ls.jump(-1)
+			end, { silent = true })
+
+			vim.keymap.set({ "i", "s" }, "<C-E>", function()
+				if ls.choice_active() then
+					ls.change_choice(1)
+				end
+			end, { silent = true })
+		end,
 	},
 	{
 		"hrsh7th/nvim-cmp",
