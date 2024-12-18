23ea0c41c191bdc7eabc73843d1564d99829f99a
Lionel Goffaux <contact@lionel-goffaux.be>
2024-12-18 18:21:11 +0100

    feat: change tab size to 4

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,7 +1,7 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
+vim.opt.expandtab = true
+vim.opt.tabstop=4
+vim.opt.softtabstop=4
+vim.opt.shiftwidth=4
 vim.g.mapleader = " "
 
 -- disable default errors
