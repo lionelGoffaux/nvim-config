496c57574903bcd2407afbbb80ffc84159f15fbc
Lionel Goffaux <lionel.goffaux@unamur.be>
2025-03-22 13:28:07 +0100

    feat: remove highlights

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,101 +1,111 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
 
-      local ensure_installed = {
-        "lua-language-server",
-        "stylua",
-        "rust-analyzer",
-        "jedi-language-server",
-        "flake8",
-        "ruff",
-        "debugpy",
-        "black",
-        "typescript-language-server",
-        "tailwindcss-language-server",
-        "eslint-lsp",
-        "prettierd",
-        "clangd",
-        "clang-format",
-        "codelldb",
-        "zls",
-        "gopls",
-        "gofumpt",
-        "goimports-reviser",
-        "golines",
-      }
+            local ensure_installed = {
+                "lua-language-server",
+                "stylua",
+                "rust-analyzer",
+                "jedi-language-server",
+                "flake8",
+                "ruff",
+                "debugpy",
+                "black",
+                "typescript-language-server",
+                "tailwindcss-language-server",
+                "eslint-lsp",
+                "prettierd",
+                "clangd",
+                "clang-format",
+                "codelldb",
+                "zls",
+                "gopls",
+                "gofumpt",
+                "goimports-reviser",
+                "golines",
+            }
 
-      vim.api.nvim_create_user_command("MasonInstallAll", function()
-        if ensure_installed and #ensure_installed > 0 then
-          vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
-        end
-      end, {})
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
+            vim.api.nvim_create_user_command("MasonInstallAll", function()
+                if ensure_installed and #ensure_installed > 0 then
+                    vim.cmd("MasonInstall " .. table.concat(ensure_installed, " "))
+                end
+            end, {})
+        end,
     },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      local lspconfig = require("lspconfig")
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.jedi_language_server.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.zls.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.gopls.setup({
-        capabilities = capabilities,
-        settings = {
-          gopls = {
-            completeUnimported = true,
-            usePlaceholders = true,
-            analyses = {
-              unusedparams = true,
-            },
-          },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        opts = {
+            auto_install = true,
         },
-      })
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local on_attach = function(client, _)
+                client.server_capabilities.semanticTokensProvider = nil -- turn off semantic tokens
+            end
+            local lspconfig = require("lspconfig")
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    end,
-  },
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.jedi_language_server.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.ts_ls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.eslint.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.zls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+
+            lspconfig.gopls.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+                settings = {
+                    gopls = {
+                        completeUnimported = true,
+                        usePlaceholders = true,
+                        analyses = {
+                            unusedparams = true,
+                        },
+                    },
+                },
+            })
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+        end,
+    },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,21 +1,21 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-  },
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
 	config = function()
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.black,
-        require("none-ls.diagnostics.ruff"),
-        require("none-ls.diagnostics.flake8"),
-        null_ls.builtins.formatting.prettierd,
-        null_ls.builtins.formatting.clang_format,
-        null_ls.builtins.formatting.gofumpt,
-        null_ls.builtins.formatting.goimports_reviser,
-        null_ls.builtins.formatting.golines,
+				null_ls.builtins.formatting.black,
+				require("none-ls.diagnostics.ruff"),
+				require("none-ls.diagnostics.flake8"),
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.clang_format,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.golines,
 			},
 		})
 
